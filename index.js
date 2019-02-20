1e37c50e4520a1d20bce5f46dbca82ee85f63492
Zooheck <davidscottflack@gmail.com>
2019-02-20 17:10:55 -0600

    Initialize migrations

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex')
+
+
+const knexConfig = {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+        filename: './data/lambda.sqlite3'
+    }
+}
+
+
+const db = knex(knexConfig);
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+server.listen(4000, () => {
+    console.log('server listening on port 4000')
+})
