b5e9992bba99501d754556de9dddc1762f96f428
Zooheck <davidscottflack@gmail.com>
2019-02-20 19:44:39 -0600

    Edit cohort

--- index.js
+++ index.js
@@ -51,6 +51,24 @@ server.post('/api/cohorts', async (req, res) => {
         res.status(500).json(err)
     }
 });
+
+server.put('/api/cohorts/:id', async (req, res) => {
+    try {
+        const count = await db('cohorts')
+            .where({ id: req.params.id })
+            .update(req.body)
+        if (count > 0) {
+            const cohort = await db('cohorts')
+                .where({ id: req.params.id })
+                .first()
+            res.status(200).json(cohort)
+        } else {
+            res.status(404).json({message: 'Zoo not found'})
+          }
+    } catch (error) {
+        res.status(500).json(err)
+    }
+});
 server.listen(4000, () => {
     console.log('server listening on port 4000')
 })
