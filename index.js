53e6c60e51ddf32a2eaa968249c551ea2fec68df
Zooheck <davidscottflack@gmail.com>
2019-02-20 18:17:05 -0600

    Get all cohorts

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-
+server.get('/api/cohorts', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts');
+        res.status(200).json(cohorts);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
 server.listen(4000, () => {
     console.log('server listening on port 4000')
 })
